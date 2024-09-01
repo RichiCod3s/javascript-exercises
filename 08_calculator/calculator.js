217424c6860710bb6a9cf99ab8a4c2e9837d0f10
RichiCod3s <108982619+RichiCod3s@users.noreply.github.com>
2024-09-01 19:50:28 +0800

    calculator method practice

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,36 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const total = arr.reduce((sum, current) => sum + current, 0 );
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const total = arr.reduce((sum, current) => sum * current, 1);
+  return total;
+};
+ 
+const power = function(x,y) {
+	const p = Math.pow(x,y);
+  return p;
 };
 
-const power = function() {
-	
+const factorial = function(num) {
+  result =1;
+  for(i=1; i<=num; i++){
+    result *=i;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
-};
-
+console.log(factorial(2));
 // Do not edit below this line
 module.exports = {
   add,
